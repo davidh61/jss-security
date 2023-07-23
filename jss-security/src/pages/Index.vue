3ded320a575d72061cb6d73d2482da9274843dd2
David Harlow <david.harlow@thetin.net>
2023-07-23 20:25:48 +0100

    fifty fifty component

--- jss-security/src/pages/Index.vue
+++ jss-security/src/pages/Index.vue
@@ -6,7 +6,7 @@
         <p>
           Our mission is to provide you bla bla bla
         </p>
-        <div class="fifty-fifty grid grid-cols-2">
+        <div class="grid md:grid-cols-2 pb-10">
           <div>
             <h2 class="text-3xl">Our services</h2>
             <ul class="mb-12 list-disc pl-4">
@@ -15,15 +15,19 @@
               <li>A simple blog with categories, tags and featured images</li>
             </ul>
           </div>
-          <div>
-          </div>
+          <g-image src="~/assets/images/cctv.jpg" width="500" />
         </div>
-        <h2 class="text-3xl">Why choose us</h2>
-        <ul class="mb-12 list-disc pl-4">
-          <li>Lorem ipsum</li>
-          <li>Image lazyloading via gridsome builtin functionality</li>
-          <li>A simple blog with categories, tags and featured images</li>
-        </ul>
+        <div class="grid md:grid-cols-2">
+          <div class="md:order-2">
+            <h2 class="text-3xl ">Why choose us</h2>
+            <ul class="mb-12 list-disc pl-4">
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+          <g-image src="~/assets/images/cctv.jpg" width="500" class="md:order-1" />
+        </div>
       </div>
     </section>
   </Layout>
