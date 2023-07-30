e313b2374d664efe02b9f21975c6422f3c14af65
David Harlow <david.harlow@thetin.net>
2023-07-30 20:48:25 +0100

    updated branding

--- jss-security/src/layouts/partials/Header.vue
+++ jss-security/src/layouts/partials/Header.vue
@@ -1,17 +1,20 @@
 <template>
   <header
     id="header"
-    class="header bg-purple-900 flex flex-col md:flex-row md:justify-between items-center p-4 md:p-6"
+    class="header bg-blue-950 flex flex-col md:flex-row md:justify-between items-center p-4 md:p-6"
     role="banner"
   >
     <g-link
       class="company-logo-link block text-white font-bold text-2xl md:text-3xl hover:text-pink-500"
       to="/"
       aria-label="Back to home"
-    >{{ $static.metadata.siteName }}</g-link>
+      ><g-image src="~/assets/images/logo-white.png" width="100" />
+    </g-link>
 
     <nav id="nav" class="nav hidden md:flex">
-      <ul class="menu flex flex-col md:flex-row items-center list-reset text-base">
+      <ul
+        class="menu flex flex-col md:flex-row items-center list-reset text-base"
+      >
         <li
           class="level-1 mb-4 md:mr-4 md:mb-0"
           v-for="element in $static.metadata.menu"
@@ -19,10 +22,11 @@
         >
           <g-link
             :to="element.link"
-            class="link font-bold text-white hover:text-pink-500"
+            class="link font-bold text-white hover:text-yellow-400"
             active-class="is-active-link"
-            exact-active-class="active text-pink-500"
-          >{{ element.name }}</g-link>
+            exact-active-class="active text-yellow-400"
+            >{{ element.name }}</g-link
+          >
         </li>
       </ul>
     </nav>
@@ -34,8 +38,8 @@ export default {
   methods: {
     socialIcon(element) {
       return `/icons.svg#icon-${element.icon}`;
-    }
-  }
+    },
+  },
 };
 </script>
 
