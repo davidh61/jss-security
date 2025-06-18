6ba3378a47ff4e84136a4598203d024b0cef13e6
David Harlow <davidharlow95@gmail.com>
2025-06-18 20:46:57 +0100

    added base path

--- jss-security/next.config.ts
+++ jss-security/next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     output: "export", 
     distDir: "github-pages",
+    basePath: '/jss-security',
+
 };
 
 export default nextConfig;
